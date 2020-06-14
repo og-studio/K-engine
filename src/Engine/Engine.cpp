78ae3849520f53076b3f84b8c4fa1606bbf4d70f
SebiTCR <Ssebi832@outlook.com>
2020-06-14 19:02:51 +0300

    Updated folder structure

---
+++ src/Engine/Engine.cpp
@@ -0,0 +1,7 @@
+#include "Engine.h"
+
+void ENGINE::Start()
+{
+    std::cout << "starting engine..." << std::endl;
+}
+
--- src/Engine/Engine.h
+++ src/Engine/Engine.h
@@ -1,8 +1,7 @@
-//
-//
-//
-
 #pragma once
+
+#include <iostream>
+
 namespace ENGINE {
 
     void Start();
